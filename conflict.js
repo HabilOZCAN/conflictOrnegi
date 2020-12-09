b851b40beb1a006ec838ddad167c18520bc214ef
HabilÖZCAN <habilozcan@gmail.com>
2020-12-09 20:32:26 +0100

    main branchde ilk uc satir degisti

--- conflict.js
+++ conflict.js
@@ -1,1 +1,4 @@
-added new line
+//Bu aciklama main brachde eklendi. mainde iken tekrar degisti
+let main = "main brachde eklendi";
+let farkliSatir = "mainde farkli satira bir degisken daha eklendi";
+
